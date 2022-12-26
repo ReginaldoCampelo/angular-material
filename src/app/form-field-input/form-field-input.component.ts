085dbbabbca3c7dd503e5635d32d05c1e1f87cd0
Reginaldo Campelo <regin.developer@gmail.com>
2022-12-26 09:18:11 -0300

    :sparkles: adding form fields component

---
+++ src/app/form-field-input/form-field-input.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field-input',
+  templateUrl: './form-field-input.component.html',
+  styleUrls: ['./form-field-input.component.scss']
+})
+export class FormFieldInputComponent implements OnInit {
+  value: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
