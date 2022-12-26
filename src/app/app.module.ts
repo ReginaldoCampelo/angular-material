5e75f44f300a98a16019f4ad0f8488f37d7f3293
Reginaldo Campelo <regin.developer@gmail.com>
2022-12-26 09:21:08 -0300

    :heavy_plus_sign: adding component imports to add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
+import { FormFieldInputComponent } from './form-field-input/form-field-input.component';
+import { FormsModule } from '@angular/forms';
+import { ProgressSpinnerComponentComponent } from './progress-spinner-component/progress-spinner-component.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { DataGridComponent } from './datagrid/datagrid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsIconsComponent,
+    FormFieldInputComponent,
+    ProgressSpinnerComponentComponent,
+    TabsComponent,
+    DataGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
