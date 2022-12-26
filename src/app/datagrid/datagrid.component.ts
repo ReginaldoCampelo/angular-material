7d0c6b848c501a7aa06e6756e0d68ec0e8f8f63e
Reginaldo Campelo <regin.developer@gmail.com>
2022-12-26 09:19:28 -0300

    :sparkles: adding datagrid component

---
+++ src/app/datagrid/datagrid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datagrid',
+  templateUrl: './datagrid.component.html',
+  styleUrls: ['./datagrid.component.scss']
+})
+export class DataGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
