599c212083f89b3c138f54e9a0fd8fcc696d06e4
Reginaldo Campelo <regin.developer@gmail.com>
2022-12-26 09:21:43 -0300

    :sparkles: adding routes for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { TabsComponent } from './tabs/tabs.component';
+import { ProgressSpinnerComponentComponent } from './progress-spinner-component/progress-spinner-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
+import { FormFieldInputComponent } from './form-field-input/form-field-input.component';
+import { DataGridComponent } from './datagrid/datagrid.component';
+
+const routes: Routes = [
+  { path: 'buttonIcon', component: ButtonsIconsComponent },
+  { path: 'formFieldInput', component: FormFieldInputComponent },
+  { path: 'progressSpinner', component: ProgressSpinnerComponentComponent},
+  { path: 'tabs', component: TabsComponent },
+  { path: 'dataGrid', component: DataGridComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
