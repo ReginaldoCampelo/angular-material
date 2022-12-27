fbddbeaa707112587e01c1f9dbf8bf0fb813d3b4
Reginaldo Campelo <regin.developer@gmail.com>
2022-12-27 15:24:59 -0300

    :seedling: improving user list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserFormComponent } from './user/user-form/user-form.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { ProgressSpinnerComponentComponent } from './progress-spinner-component/progress-spinner-component.component';
 import { NgModule } from '@angular/core';
@@ -6,14 +7,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
 import { FormFieldInputComponent } from './form-field-input/form-field-input.component';
 import { DataGridComponent } from './datagrid/datagrid.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 const routes: Routes = [
   { path: 'buttonIcon', component: ButtonsIconsComponent },
   { path: 'formFieldInput', component: FormFieldInputComponent },
-  { path: 'progressSpinner', component: ProgressSpinnerComponentComponent},
+  { path: 'progressSpinner', component: ProgressSpinnerComponentComponent },
   { path: 'tabs', component: TabsComponent },
   { path: 'dataGrid', component: DataGridComponent },
-  { path: '', redirectTo: '/dataGrid', pathMatch: 'full' }
+  { path: 'user-list', component: UserListComponent },
+  { path: 'user-form', component: UserFormComponent },
+  { path: 'user-form/:id', component: UserFormComponent },
+  { path: '', redirectTo: '/user-list', pathMatch: 'full' }
 ];
 
 @NgModule({
