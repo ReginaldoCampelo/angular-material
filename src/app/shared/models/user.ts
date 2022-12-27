f0cef44189dd2ceca04eefd258159ee304b0f95f
Reginaldo Campelo <regin.developer@gmail.com>
2022-12-27 16:10:42 -0300

    adding user class

---
+++ src/app/shared/models/user.ts
@@ -0,0 +1,16 @@
+export class User {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  cpf: string;
+  cnh: string;
+  birthDay: Date;
+  sex: string;
+  phone: string;
+  address: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+}
